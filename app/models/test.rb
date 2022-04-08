b3e73d9101927ad3ac39c031a456a1707273726d
KRezOl <olegrez@tut.by>
2022-04-08 22:12:19 +0200

    Added method <tests_by_category(category)> for selecting tests by category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  class << self
+    def tests_by_category(category)
+      Test.where(category_id: (Category.where(title: category)).map { |c| c.id })
+    end
+  end
 end
