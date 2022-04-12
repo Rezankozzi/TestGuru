8f9edb871e7954409c00f3d763197970dd95b918
Rezankozzi <88398170+Rezankozzi@users.noreply.github.com>
2022-04-12 21:39:54 +0200

    Update test.rb
    Add the JOINS operator.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,9 @@
 class Test < ApplicationRecord
   class << self
     def tests_by_category(category)
-      Test.order(created_at::desc)
-          .where(category_id: (Category.where(title: category)).pluck(:id))
+      Test.order('tests.title asc')
+          .joins('JOIN categories ON tests.category_id = categories.id')
+          .where(categories: {title: category})
           .pluck(:title)
     end
   end
