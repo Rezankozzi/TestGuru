be24b48ee7866f7ae49404b8c8bf1a822ee733c2
Rezankozzi <88398170+Rezankozzi@users.noreply.github.com>
2022-04-12 22:21:22 +0200

    Update 20220407185223_create_user_tests.rb

--- db/migrate/20220407185223_create_user_tests.rb
+++ db/migrate/20220407185223_create_user_tests.rb
@@ -4,8 +4,8 @@ class CreateUserTests < ActiveRecord::Migration[7.0]
       t.string :status, default: 'in progress'
       t.references :user, foreign_key: true
       t.references :test, foreign_key: true
-      t.datetime :started_at
-      t.datetime :finished_at, default: 0
+      
+      t.timestamps
     end
   end
 end
