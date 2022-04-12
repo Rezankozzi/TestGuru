5e98a98256b876a3e85c62b767df71a9dd6863d3
Rezankozzi <88398170+Rezankozzi@users.noreply.github.com>
2022-04-12 21:41:43 +0200

    Update user.rb
    Add the JOINS operator.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.where(id: UserTest.where(user_id: self.id).pluck(:test_id), level: level)
+     Test.joins('JOIN user_tests ON  user_tests.test_id = tests.id')
+        .where(level: level, user_tests: {user_id: self.id})
+        .pluck(:title)
   end
 end
