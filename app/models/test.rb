ac0be3b28d59b0755aee2fe7acccd67114e799c7
KRezOl <olegrez@tut.by>
2022-05-07 21:19:10 +0200

    Add associations to each model, change method <tests_by_level>

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,14 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  belongs_to :author, class_name: 'User'
+  has_many :user_tests
+  has_many :users, through: :user_tests
   class << self
     def tests_by_category(category)
       Test.order('tests.title asc')
           .joins('JOIN categories ON tests.category_id = categories.id')
-          .where(categories: {title: category})
+          .where(categories: { title: category })
           .pluck(:title)
     end
   end
