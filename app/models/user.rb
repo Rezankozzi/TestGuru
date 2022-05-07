195252ffff3ee638d4f7625e9d9f0554083f07ba
KRezOl <olegrez@tut.by>
2022-05-07 20:01:31 +0200

    Add associations to all  models; change method <tests_by_level> in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :user_tests
+  has_many :tests, through: :user_tests
   def tests_by_level(level)
-     Test.joins('JOIN user_tests ON  user_tests.test_id = tests.id')
-        .where(level: level, user_tests: {user_id: self.id})
-        .pluck(:title)
+    tests.where(level: level)
   end
 end
